aa7bfe4ef021fa18f853bc312ab27c4201e44244
Pierre-Bossis <pbossis@hotmail.com>
2022-08-04 21:03:42 +0200

    update pagination doctrine

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -8,16 +8,44 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('personne')]
+
 class PersonneController extends AbstractController
 {
-    #[Route('/personne/add', name: 'personne.add')]
+    #[Route('/', name:'personne.list')]
+    public function index(ManagerRegistry $doctrine) : Response{
+        $repository = $doctrine->getRepository(Personne::class);
+       $personnes = $repository->findAll();
+       return $this->render('personne/index.html.twig',['personnes'=>$personnes]);
+    }
+
+    #[Route('/{id<\d+>}', name:'personne.detail')]
+    public function detail(Personne $personne = null) : Response{ //$id
+      //  $repository = $doctrine->getRepository(Personne::class);
+      //  $personne = $repository->find($id);
+        if(!$personne){
+            $this->addFlash('error',"La personne n'existe pas.");
+            return $this->redirectToRoute('personne.list');
+        }
+
+       return $this->render('personne/detail.html.twig',['personne'=>$personne]);
+    }
+
+    #[Route('/alls/{page?1}/{nbre?12}', name:'personne.list.alls')]
+    public function indexAlls(ManagerRegistry $doctrine, $page, $nbre) : Response{
+        $repository = $doctrine->getRepository(Personne::class);
+       $personnes = $repository->findBy([],[], $nbre,($page -1)* $nbre);
+       return $this->render('personne/index.html.twig',['personnes'=>$personnes]);
+    }
+
+    #[Route('/add', name: 'personne.add')]
     public function addPersonne(ManagerRegistry $doctrine): Response
     {
         $entityManager = $doctrine->getManager();
         $personne = new Personne();
-        $personne->setFirstname('Pierre');
+        $personne->setFirstname('Caroline');
         $personne->setName('Bossis');
-        $personne->setAge('27');
+        $personne->setAge('29');
 
  //       $personne2 = new Personne();
  //       $personne2->setFirstname('Nicolas');
