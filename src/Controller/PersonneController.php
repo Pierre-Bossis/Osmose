c57103f0a6f92b1fd9b0847cdb20c210bd97a14f
Pierre-Bossis <pbossis@hotmail.com>
2022-08-05 23:46:35 +0200

    update pagination doctrine + query builder

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Personne;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,9 +16,18 @@ class PersonneController extends AbstractController
     #[Route('/', name:'personne.list')]
     public function index(ManagerRegistry $doctrine) : Response{
         $repository = $doctrine->getRepository(Personne::class);
-       $personnes = $repository->findAll();
-       return $this->render('personne/index.html.twig',['personnes'=>$personnes]);
+    $personnes = $repository->findAll();
+    return $this->render('personne/index.html.twig',['personnes'=>$personnes]);
     }
+
+    #[Route('/alls/age/{ageMin}/{ageMax}', name:'personne.list.age')]
+    public function personneByAge(ManagerRegistry $doctrine, $ageMin, $ageMax) : Response{
+        $repository = $doctrine->getRepository(Personne::class);
+    $personnes = $repository->findPersonnesByAgeInterval($ageMin,$ageMax);
+    return $this->render('personne/index.html.twig',['personnes'=>$personnes]);
+    }
+
+
 
     #[Route('/{id<\d+>}', name:'personne.detail')]
     public function detail(Personne $personne = null) : Response{ //$id
@@ -34,8 +44,17 @@ class PersonneController extends AbstractController
     #[Route('/alls/{page?1}/{nbre?12}', name:'personne.list.alls')]
     public function indexAlls(ManagerRegistry $doctrine, $page, $nbre) : Response{
         $repository = $doctrine->getRepository(Personne::class);
-       $personnes = $repository->findBy([],[], $nbre,($page -1)* $nbre);
-       return $this->render('personne/index.html.twig',['personnes'=>$personnes]);
+        $nbPersonne = $repository->count([]);
+        $nbPage = ceil($nbPersonne / $nbre);
+        $personnes = $repository->findBy([],[], $nbre,($page -1)* $nbre);
+
+       return $this->render('personne/index.html.twig',[
+        'personnes'=>$personnes,
+         'isPaginated'=>true,
+         'nbPage'=>$nbPage,
+         'page'=>$page,
+         'nbre'=>$nbre
+    ]);
     }
 
     #[Route('/add', name: 'personne.add')]
@@ -62,5 +81,44 @@ class PersonneController extends AbstractController
         return $this->render('personne/detail.html.twig', [
             'personne' => $personne,
         ]);
+    }
+
+    #[Route('/delete/{id}', name:'personne.delete')]
+    public function deletePersonne(Personne $personne = null, ManagerRegistry $doctrine): RedirectResponse{
+        // récuperer la personne
+        if($personne){
+            // si la personne existe => le supprimer et retourner un flashMessage de succès
+            $manager = $doctrine->getManager();
+            //ajoute la fonction de suppression dans la transaction
+            $manager->remove($personne);
+            //executer la transaction
+            $manager->flush();
+            $this->addFlash('success',"La personne a été supprimée avec succès.");
+        } else{
+        //sinon => retourner un flashMessage d'erreur
+            $this->addFlash('error',"Personne inexistante.");
+        }
+        return $this->redirectToRoute('personne.list.alls');
+
+
+    }
+
+    #[Route('/update/{id}/{name}/{firstname}/{age}', name:'personne.update')]
+    public function updatePersonne(Personne $personne = null, $name, $firstname, $age, ManagerRegistry $doctrine): RedirectResponse{
+        // vérifier que la personne à mettre à jour existe.
+        if($personne){
+            // si la personne existe => mettre à jour notre personne + message succès
+            $personne->setName($name);
+            $personne->setFirstname($firstname);
+            $personne->setAge($age);
+            $manager = $doctrine->getManager();
+            $manager->persist($personne);
+            $manager->flush();
+            $this->addFlash('success','La personne a été mise à jour avec succès.');
+        } else{
+            // sinon => déclencher message d'erreur
+            $this->addFlash('error',"Personne inexistante.");
+        }
+        return $this->redirectToRoute('personne.list.alls');
     }
 }
--- src/Repository/PersonneRepository.php
+++ src/Repository/PersonneRepository.php
@@ -42,17 +42,19 @@ class PersonneRepository extends ServiceEntityRepository
 //    /**
 //     * @return Personne[] Returns an array of Personne objects
 //     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findPersonnesByAgeInterval($ageMin, $ageMax)
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.age >= :ageMin and p.age <= :ageMax')
+            ->setParameters(['ageMin'=>$ageMin,'ageMax'=>$ageMax])
+//            ->setParameter('ageMin', $ageMin)
+//            ->setParameter('ageMax', $ageMax)
+//           ->orderBy('p.id', 'ASC')
+//         ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 
 //    public function findOneBySomeField($value): ?Personne
 //    {
